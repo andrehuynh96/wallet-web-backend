ecc09638076b9ddad2d45e144272786085b96f6f
Vuong Thang <thangdv@blockchainlabs.asia>
2020-02-06 11:30:47 +0700

    swagger and login

--- app/feature/login/login.controller.js
+++ app/feature/login/login.controller.js
@@ -1,11 +1,82 @@
 const logger = require('app/lib/logger');
-
+const Member = require('app/model/wallet').members;
+const MemberActivityLog = require('app/model/wallet').member_activity_logs;
+const OTP = require('app/model/wallet').otps;
+const MemberStatus = require('app/model/wallet/value-object/member-status');
+const ActionType = require('app/model/wallet/value-object/member-activity-action-type');
+const OtpType = require('app/model/wallet/value-object/otp-type');
+const memberMapper = require('app/feature/response-schema/member.response-schema');
+const bcrypt = require('bcrypt');
+const config = require("app/config");
+const uuidV4 = require('uuid/v4');
 module.exports = async (req, res, next) => {
   try {
-    let user = null;// implement login business
-    req.session.authenticated = true;
-    req.session.user = user;
-    return res.ok(user);
+    let user = await Member.findOne({
+      where: {
+        email: req.body.email,
+        deleted_flg: false
+      }
+    });
+    if (!user) {
+      return res.badRequest(res.__("USER_NOT_FOUND"), "USER_NOT_FOUND", { fields: ["email"] });
+    }
+    const match = await bcrypt.compare(req.body.password, user.password_hash);
+    if (!match) {
+      return res.unauthorized(res.__("LOGIN_FAIL", "LOGIN_FAIL"));
+    }
+
+    if (user.status == MemberStatus.UNACTIVATED) {
+      return res.forbidden(res.__("UNCONFIRMED_ACCOUNT", "UNCONFIRMED_ACCOUNT"));
+    }
+
+    if (user.status == MemberStatus.LOCKED) {
+      return res.forbidden(res.__("ACCOUNT_LOCKED", "ACCOUNT_LOCKED"));
+    }
+    if (user.twofa_enable_flg) {
+      let verifyToken = Buffer.from(uuidV4()).toString('base64');
+      let today = new Date();
+      today.setHours(today.getHours() + config.expiredVefiryToken);
+
+      await OTP.update({
+        expired: true
+      }, {
+          where: {
+            member_id: user.id,
+            action_type: OtpType.TWOFA
+          },
+          returning: true
+        })
+
+      await OTP.create({
+        code: verifyToken,
+        used: false,
+        expired: false,
+        expired_at: today,
+        member_id: user.id,
+        action_type: OtpType.TWOFA
+      })
+
+      return res.ok({
+        twofa: true,
+        verify_token: verifyToken
+      });
+    }
+    else {
+      const registerIp = (req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.headers['x-client'] || req.ip).replace(/^.*:/, '');
+      await MemberActivityLog.create({
+        member_id: user.id,
+        client_ip: registerIp,
+        action: ActionType.LOGIN,
+        user_agent: req.headers['user-agent']
+      });
+
+      req.session.authenticated = true;
+      req.session.user = user;
+      return res.ok({
+        twofa: false,
+        user: memberMapper(user)
+      });
+    }
   }
   catch (err) {
     logger.error("login fail: ", err);
--- app/feature/login/login.route.js
+++ app/feature/login/login.route.js
@@ -4,8 +4,6 @@ const verifyRecaptcha = require('app/middleware/verify-recaptcha.middleware');
 const validator = require('app/middleware/validator.middleware');
 const loginRequestSchema = require('./login.request-schema');
 const loginController = require('./login.controller');
-
-console.log(config)
 const Recaptcha = require('express-recaptcha').RecaptchaV2;
 const recaptcha = new Recaptcha(config.recaptchaSiteKey, config.recaptchaSecret);
 const router = express.Router();
@@ -19,3 +17,68 @@ router.post(
 );
 
 module.exports = router;
+
+/*********************************************************************/
+
+/**
+ * @swagger
+ * /web/login:
+ *   post:
+ *     summary: Login
+ *     tags:
+ *       - Accounts
+ *     description: if twofa == true then return verify_token otherwise return user object
+ *     parameters:
+ *       - in: body
+ *         name: data
+ *         description: Data for login.
+ *         schema:
+ *            type: object
+ *            required:
+ *            - g-recaptcha-response
+ *            - email
+ *            - password
+ *            example:
+ *               {
+                        "g-recaptcha-response":"3f76680510bcca07e7e011dcc1effb079d1d0a34",
+                        "email":"example@gmail.com",
+                        "password":"abc123456"
+                  }
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         examples:
+ *           application/json:
+ *             {
+ *                 "data":{
+                      "twofa":true,
+                      "verify_token":"3f76680510bcca07e7e011dcc1effb079d1d0a34",
+                      "user":{
+                        "id": 1,
+                        "email":"example@gmail.com",
+                        "twofa_secret":"sCM87xx",
+                        "twofa_enable_flg": true,
+                        "create_at":"",
+                        "member_sts":"ACTIVATED"
+                      }
+                    }
+ *             }
+ *       400:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/400'
+ *       401:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       404:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/404'
+ *       500:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */

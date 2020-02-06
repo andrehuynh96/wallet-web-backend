c605e2cee3019b79f1cec9eb887832add4026d26
Unknown <huyht@blockchainlabs.asia>
2020-02-06 15:20:23 +0700

    route register
    route register

--- app/feature/register/register.route.js
+++ app/feature/register/register.route.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const validator = require("app/middleware/validator.middleware");
 const schema = require("./register.request-schema");
+const controller = require('./register.controller');
+const Recaptcha = require('express-recaptcha').RecaptchaV2;
+const recaptcha = new Recaptcha(config.recaptchaSiteKey, config.recaptchaSecret);
 const router = express.Router();
+
+router.post(
+  '/register',
+  validator(schema),
+  recaptcha.middleware.verify,
+  verifyRecaptcha,
+  controller
+);
 
 
 module.exports = router;

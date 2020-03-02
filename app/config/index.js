1d89b5a4b6c22ff5ba95555c348f40d889994d3b
Huy Hoang <huyht@blockchainlabs.asia>
2020-03-02 11:00:50 +0800

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -78,7 +78,7 @@ const config = {
   website: {
     url: process.env.WEBSITE_URL,
     urlActive: process.env.WEBSITE_URL + '/sign-in?token=',
-    urlUnsubcribe: process.env.WEBSITE_URL +'confirm-unsubcribe'
+    urlUnsubcribe: process.env.WEBSITE_URL +'/confirm-unsubcribe'
   },
   aws: {
     endpoint: process.env.AWS_END_POINT,

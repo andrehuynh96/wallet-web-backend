fc46603b58387afa2daa3341c000733d9c11552c
Vuong Thang <thangdv@blockchainlabs.asia>
2020-03-06 10:09:22 +0700

    option 3

--- app/feature/wallet/validator/create.js
+++ app/feature/wallet/validator/create.js
@@ -1,8 +1,7 @@
 const Joi = require('joi');
 
 const schema = Joi.object().keys({
-  passphrase_hash: Joi.string().required(),
-  password_hash: Joi.string().required(),
+  encrypted_passphrase: Joi.string().required(),
   default_flg: Joi.boolean().optional()
 });
 
--- app/feature/wallet/wallet-private-key/validator/create.js
+++ app/feature/wallet/wallet-private-key/validator/create.js
@@ -3,13 +3,12 @@ const Joi = require('joi');
 const schema = Joi.object().keys({
   items: Joi.array().required().items(
     Joi.object().keys({
-      private_key_hash: Joi.string().required(),
+      encrypted_private_key: Joi.string().required(),
       platform: Joi.string().required(),
       address: Joi.string().required(),
       hd_path: Joi.string().required()
     })
-  ),
-  password_hash: Joi.string().required()
+  )
 });
 
 module.exports = schema;

96196ccaef1b3f92f31c0ed2ac1424fd18d85b3b
ducnt <ducnt@blockchainlabs.asia>
2020-06-17 12:40:41 +0700

    add source implement membership reward

---
+++ app/feature/response-schema/membership/reward.response-schema.js
@@ -0,0 +1,24 @@
+const objectMapper = require('object-mapper');
+
+const destObject = {
+  array: {
+    '[].currency': '[].currency',
+    '[].amount': '[].amount'
+  },
+  single: {
+    currency: 'currency',
+    amount: 'amount'
+  }
+};
+module.exports = srcObject => {
+  if (Array.isArray(srcObject)) {
+    if (srcObject === undefined || srcObject.length == 0) {
+      return srcObject;
+    } else {
+      return objectMapper(srcObject, destObject.array);
+    }
+  }
+  else {
+    return objectMapper(srcObject, destObject.single);
+  }
+}; 

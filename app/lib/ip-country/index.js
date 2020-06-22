6f62b210cd9a8c34696ac3603c5922d738e14509
ducnt <ducnt@blockchainlabs.asia>
2020-06-22 19:17:36 +0700

    fixed change lib get country

--- app/lib/ip-country/index.js
+++ app/lib/ip-country/index.js
@@ -1,10 +1,12 @@
 const logger = require('app/lib/logger');
 const Axios = require('axios');
 const config = require('app/config');
+const publicIp = require('public-ip');
 module.exports = {
     getCountryLocal: async () => {
         try {
-            return await Axios.get('http://ip-api.com/json')
+            const _ip = await publicIp.v4();
+            return await Axios.get(`https://freegeoip.app/json/${_ip}`);
         }catch (err) {
             logger.error("getCountryLocal: ", err);
             throw err;
@@ -12,9 +14,10 @@ module.exports = {
     },
     isAllowCountryLocal: async () => {
         try {
-            const _country = await Axios.get('http://ip-api.com/json');
+            const _ip = await publicIp.v4();
+            const _country = await Axios.get(`https://freegeoip.app/json/${_ip}`);
             const _CountryWhitelist = config.membership.countryWhitelist.split(',')
-            return _CountryWhitelist.indexOf(_country.data.countryCode) > -1;
+            return _CountryWhitelist.indexOf(_country.data.country_code) > -1;
         }catch (err) {
             logger.error("isExistCountryLocal: ", err);
             throw err;

4ec63d694b7fe4cee014188183b8823b8e2a4399
nqt900 <nqt900@gmail.com>
2020-07-02 18:23:21 +0700

    return data

--- app/feature/member/list-referral/list-referral.controller.js
+++ app/feature/member/list-referral/list-referral.controller.js
@@ -1,13 +1,13 @@
 const logger = require("app/lib/logger");
 const Member = require("app/model/wallet").members;
+const MembershipType = require("app/model/wallet").membership_types;
 const MemberStatus = require('app/model/wallet/value-object/member-status');
-const Affiliate = require('app/lib/reward-system/affiliate');
 
 module.exports = async (req, res, next) => {
   try {
     let member = await Member.findOne({
       where: {
-        id: req.user.id
+        id: '8df81656-0ddb-472c-956e-b46a11bb9450' //req.user.id
       }
     });
 
@@ -24,30 +24,39 @@ module.exports = async (req, res, next) => {
     }
     let limit = req.query.limit ? parseInt(req.query.limit) : 10;
     let offset = req.query.offset ? parseInt(req.query.offset) : 0;
-    const { count: total, rows: items } = await CosmosAccountContribution.findAndCountAll({
+    const { count: total, rows: items } = await Member.findAndCountAll({
       limit,
       offset,
-      attributes:['id', 'email', 'member_sts', 'fullname', 'kyc_level', 'kyc_status'],
+      attributes:['id', 'email', 'member_sts', 'fullname', 'kyc_level', 'kyc_status', 'createdAt'],
       include: [{
         model: MembershipType,
+        as: 'MembershipType', 
         attributes: ['id','name']
       }],
       where: {
         referrer_code : member.referral_code,
         deleted_flg: false
-      }
+      },
+      raw: true
     })
+    result = []
     items.forEach((item) => {
-      if(item.MembershipType){
-        item.membership_name= item.MembershipType.name
-        item.membership_id = item.MembershipType.id
-      }
-      delete item.MembershipType
+      result.push({
+        id: item.id,
+        member_sts: item.member_sts,
+        email: item.email,
+        fullname: item.fullname,
+        kyc_level: item.kyc_level,
+        kyc_status: item.kyc_status,
+        membership_type_id: item['MembershipType.id'],
+        membership_type_name: item['MembershipType.name'],
+        created_at: item.createdAt
+      })
     })
     return res.ok({
       offset: offset,
       limit: limit,
-      items: items,
+      items: result,
       total: total
     });
   }

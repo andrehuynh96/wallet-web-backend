2fb92058e3614cef064c8248beccb5de4c699efb
nqt900 <nqt900@gmail.com>
2020-07-02 17:42:03 +0700

    get list referral

--- app/feature/member/list-referral/list-referral.controller.js
+++ app/feature/member/list-referral/list-referral.controller.js
@@ -24,8 +24,32 @@ module.exports = async (req, res, next) => {
     }
     let limit = req.query.limit ? parseInt(req.query.limit) : 10;
     let offset = req.query.offset ? parseInt(req.query.offset) : 0;
-    let result = await Affiliate.getReferrals({ email: member.email, offset: offset, limit: limit });
-    return res.status(result.httpCode).send(result.data);
+    const { count: total, rows: items } = await CosmosAccountContribution.findAndCountAll({
+      limit,
+      offset,
+      attributes:['id', 'email', 'member_sts', 'fullname', 'kyc_level', 'kyc_status'],
+      include: [{
+        model: MembershipType,
+        attributes: ['id','name']
+      }],
+      where: {
+        referrer_code : member.referral_code,
+        deleted_flg: false
+      }
+    })
+    items.forEach((item) => {
+      if(item.MembershipType){
+        item.membership_name= item.MembershipType.name
+        item.membership_id = item.MembershipType.id
+      }
+      delete item.MembershipType
+    })
+    return res.ok({
+      offset: offset,
+      limit: limit,
+      items: items,
+      total: total
+    });
   }
   catch (err) {
     logger.error('create link kyc fail:', err);

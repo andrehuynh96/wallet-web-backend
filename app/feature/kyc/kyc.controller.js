c461ba3bb38519c455ff4d964066edee2aa3f1e0
huyht-ibl <huyht@blockchainlabs.asia>
2020-08-05 17:49:27 +0700

    check KYC level 2 DECLINED cannot edit

--- app/feature/kyc/kyc.controller.js
+++ app/feature/kyc/kyc.controller.js
@@ -166,13 +166,13 @@ module.exports = {
         kyc_status: memberKyc.status,
         ...memberData
       }, {
-        where: {
-          id: req.user.id
-        },
-        returning: true,
-        plain: true,
-        transaction: transaction
-      });
+          where: {
+            id: req.user.id
+          },
+          returning: true,
+          plain: true,
+          transaction: transaction
+        });
 
       if (memberKyc.status == KycStatus.APPROVED &&
         kyc.approve_membership_type_id &&
@@ -236,7 +236,8 @@ module.exports = {
     let transaction;
     try {
       if (req.user.kyc_level == "LEVEL_2" &&
-        req.user.kyc_status == KycStatus.APPROVED) {
+        (req.user.kyc_status == KycStatus.APPROVED ||
+          req.user.kyc_status == KycStatus.DECLINED)) {
         return res.badRequest(res.__("KYC_HAVE_BEEN_FINISHED"), "KYC_HAVE_BEEN_FINISHED");
       }
       let kyc = await Kyc.findOne({
@@ -321,27 +322,27 @@ module.exports = {
             value: i.value,
             note: i.note,
           }, {
-            where: {
-              member_kyc_id: i.member_kyc_id,
-              property_id: i.property_id,
-            },
-            returning: true,
-            plain: true,
-            transaction: transaction
-          });
+              where: {
+                member_kyc_id: i.member_kyc_id,
+                property_id: i.property_id,
+              },
+              returning: true,
+              plain: true,
+              transaction: transaction
+            });
         }
       }
 
       let [_, response] = await Member.update({
         ...memberData
       }, {
-        where: {
-          id: req.user.id
-        },
-        returning: true,
-        plain: true,
-        transaction: transaction
-      });
+          where: {
+            id: req.user.id
+          },
+          returning: true,
+          plain: true,
+          transaction: transaction
+        });
       req.session.user = response;
       await transaction.commit();
       return res.ok(true);

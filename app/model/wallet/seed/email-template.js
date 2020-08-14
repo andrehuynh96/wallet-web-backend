6bffce655750f6d79a0d7bf9a2ecfd7eb82a1cfa
myhn <myhn@blockchainlabs.asia>
2020-08-14 10:52:16 +0700

    update seed email template

--- app/model/wallet/seed/email-template.js
+++ app/model/wallet/seed/email-template.js
@@ -1,5 +1,6 @@
 const EmailTemplate = require('app/model/wallet').email_templates;
 const EmailTemplateTypes = require('app/model/wallet/value-object/email-template-type');
+const EmailTemplateDisplayName = require('app/model/wallet/value-object/email-template-display-name');
 const fs = require('fs');
 const logger = require('app/lib/logger');
 const path = require("path");
@@ -13,26 +14,32 @@ module.exports = async () => {
     name: EmailTemplateTypes.TRANSACTION_RECEIVED,
     subject: 'Received coin/token alert',
     template: fs.readFileSync(path.join(root, 'transaction-received.ejs'), 'utf-8'),
+    display_name: EmailTemplateDisplayName.TRANSACTION_RECEIVED
   }, {
     name: EmailTemplateTypes.TRANSACTION_SENT,
     subject: 'Send coin/token alert`',
     template: fs.readFileSync(path.join(root, 'transaction-sent.ejs'), 'utf-8'),
+    display_name: EmailTemplateDisplayName.TRANSACTION_SENT
   }, {
     name: EmailTemplateTypes.VERIFY_EMAIL,
     subject: 'Create Account',
     template: fs.readFileSync(path.join(root, 'verify-email.ejs'), 'utf-8'),
+    display_name: EmailTemplateDisplayName.VERIFY_EMAIL
   }, {
     name: EmailTemplateTypes.RESET_PASSWORD,
     subject: 'Reset Password',
     template: fs.readFileSync(path.join(root, 'reset-password.ejs'), 'utf-8'),
+    display_name: EmailTemplateDisplayName.RESET_PASSWORD
   }, {
     name: EmailTemplateTypes.DEACTIVE_ACCOUNT,
     subject: 'Delete Account',
     template: fs.readFileSync(path.join(root, 'deactive-account.ejs'), 'utf-8'),
+    display_name: EmailTemplateDisplayName.DEACTIVE_ACCOUNT
   }, {
     name: EmailTemplateTypes.REFERRAL,
     subject: 'Invitation Email',
     template: fs.readFileSync(path.join(root, 'referral.ejs'), 'utf-8'),
+    display_name: EmailTemplateDisplayName.REFERRAL
   },
   ];
 
---
+++ app/model/wallet/value-object/email-template-display-name.js
@@ -0,0 +1,8 @@
+module.exports = {
+    TRANSACTION_RECEIVED: "Web wallet transaction received",
+    TRANSACTION_SENT: "Web wallet transaction sent",
+    VERIFY_EMAIL: "Web wallet verify email",
+    RESET_PASSWORD: 'Web wallet reset password',
+    DEACTIVE_ACCOUNT: 'Web wallet deactive account',
+    REFERRAL: "Web wallet referral"
+};

28ebcfe80138b404cde0c1c26f5b64f198bcb49c
Huy <huyht@blockchainlabs.asia>
2020-09-01 17:39:45 +0700

    get price coingeki\o

--- app/lib/coin-gecko-client/index.js
+++ app/lib/coin-gecko-client/index.js
@@ -2,13 +2,19 @@ const logger = require('app/lib/logger');
 const Platform = require('app/model/wallet/value-object/platform');
 const CoinGecko = require('coingecko-api');
 const redis = require("app/lib/redis");
+const { StepFunctions } = require('aws-sdk');
 const cache = redis.client();
+const mappingCoin = {
+  "BTCSW": "BTC"
+}
+
 module.exports = {
   getPrice: async ({ platform_name, currency }) => {
     try {
       if (platform_name == "USDT") {
         return 1;
       }
+      platform_name = _getplatform(platform_name);
       logger.info('getPrice: getPrice');
       const key = platform_name + '_' + currency;
       let price = await cache.getAsync(key);
@@ -29,4 +35,12 @@ module.exports = {
       throw err;
     }
   }
+}
+
+function _getplatform(platform) {
+  if (mappingCoin[platform]) {
+    return mappingCoin[platform];
+  }
+
+  return platform;
 }

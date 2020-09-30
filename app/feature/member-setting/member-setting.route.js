d606736b7a0b73706af86315ab638b24d2fd71c5
Pham Ba Luan <luanpb@cardano-labo.com>
2020-09-30 10:53:06 +0700

    Api get & update member-setting.

---
+++ app/feature/member-setting/member-setting.route.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const controller = require('./member-setting.controller');
+const authenticate = require('app/middleware/authenticate.middleware');
+const router = express.Router();
+
+router.get(
+  '/member-settings/:memberId',
+   authenticate,
+  controller.get
+);
+module.exports = router;
+
+router.put(
+  '/member-settings/:memberId',
+   authenticate,
+  controller.update
+);
+module.exports = router;
+
+
+/** *******************************************************************/
+
+/**
+ * @swagger
+ * /web/member-settings/{memberId}:
+ *   get:
+ *     summary: Get member setting
+ *     tags:
+ *       - Setting
+ *     description: Get member setting
+ *     parameters:
+ *       - name: memberId
+ *         in: query
+ *         type: integer
+ *         format: int32
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Ok
+ *         examples:
+ *           application/json:
+ *             {
+  *       "data": {
+  *           "member_id": "a87548d2-6275-4001-a0b8-20752f178710",
+  *           "is_receiced_system_notification_flg": true,
+  *            "is_receiced_activity_notification_flg": true,
+  *            "is_receiced_news_notification_flg": true,
+  *            "is_receiced_marketing_notification_flg": true,
+  *            "created_at": "2020-09-29T06:13:21.033Z",
+  *            "updated_at": "2020-09-29T06:13:21.033Z"
+  *       }
+ *             }
+ *       400:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/400'
+ *       401:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       404:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/404'
+ *       500:
+ *         description: Error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
+
+/**
+* @swagger
+* /web/member-settings/{memberId}:
+*   put:
+*     summary: update member setting
+*     tags:
+*       - Setting
+*     description: update member setting
+*     parameters:
+*       - name: memberId
+*         in: query
+*         type: integer
+*         format: int32
+*       - in: body
+*         name: data
+*         description: Data submit for update member setting.
+*         schema:
+*            type: object
+*            example:
+*             {
+*                	"is_receiced_system_notification_flg": false,
+*                  "is_receiced_activity_notification_flg": false,
+*                  "is_receiced_news_notification_flg": false,
+*                  "is_receiced_marketing_notification_flg": true
+*             }
+*     responses:
+*       200:
+*         description: Ok
+ *         examples:
+ *           application/json:
+ *             {
+ *                 "data": true
+ *             }
+*       400:
+*         description: Error
+*         schema:
+*           $ref: '#/definitions/400'
+*       401:
+*         description: Error
+*         schema:
+*           $ref: '#/definitions/401'
+*       404:
+*         description: Error
+*         schema:
+*           $ref: '#/definitions/404'
+*       500:
+*         description: Error
+*         schema:
+*           $ref: '#/definitions/500'
+*/
---
+++ app/model/wallet/member-setting.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const Model = sequelize.define("member_settings", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    member_id: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    is_receiced_system_notification_flg: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    is_receiced_activity_notification_flg: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    is_receiced_news_notification_flg: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    is_receiced_marketing_notification_flg: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  }, {
+    underscored: true,
+    timestamps: true,
+  });
+
+  Model.associate = (models) => {
+    Model.belongsTo(models.members, {
+      as: 'Member',
+      foreignKey: 'member_id',
+      targetKey: 'id'
+    });
+  };
+
+  return Model;
+};

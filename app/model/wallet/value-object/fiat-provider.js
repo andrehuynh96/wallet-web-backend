90e4ab03e478968e9655b50fa559aac5bbcdeb40
Unknown <huyht@blockchainlabs.asia>
2020-11-04 17:52:55 +0700

    add model nexo
    add model nexo

--- app/model/wallet/value-object/fiat-provider.js
+++ app/model/wallet/value-object/fiat-provider.js
@@ -1,3 +1,7 @@
+const PointService = require('app/lib/point');
+const PointService = require('app/lib/point');
+const PointService = require('app/lib/point');
+const PointService = require('app/lib/point');
 module.exports = {
   'SENDWYRE': 'SENDWYRE'
 }

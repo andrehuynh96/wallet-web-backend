0cdd53d2267aedb324a1b9b2b08ceb87636a44eb
ducTD <ductd@blockchainlabs.asia>
2020-11-09 16:39:41 +0700

    stak 3832-3911

--- app/feature/bank/nexo/member/validator/index.js
+++ app/feature/bank/nexo/member/validator/index.js
@@ -1,7 +1,11 @@
 const create = require('./create');
 const verify = require('./verify');
+const recovery_request = require('./recovery-reqquest');
+const recovery_verify = require('./recovery-verify');
 
 module.exports = {
   create: create,
-  verify: verify
+  verify: verify,
+  recovery_request: recovery_request,
+  recovery_verify: recovery_verify
 };

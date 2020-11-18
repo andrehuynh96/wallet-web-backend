fa7becee2ab33bf95c57f5affa515301b90f8895
huyht-ibl <huyht@blockchainlabs.asia>
2020-11-18 18:09:34 +0700

    update swagger

--- app/feature/survey/survey.route.js
+++ app/feature/survey/survey.route.js
@@ -37,9 +37,6 @@ module.exports = router;
                     "description": "",
                     "point": 100,
                     "estimate_time": 60,
-                    "deleted_flg": false,
-                    "created_by": 32,
-                    "updated_by": 32,
                     "created_at": "2020-11-18T04:52:39.884Z",
                     "updated_at": "2020-11-18T04:52:39.884Z",
                     "questions": [
@@ -50,20 +47,15 @@ module.exports = router;
                         "category_type": "ANSWER_NOW",
                         "points": 0,
                         "estimate_time": 0,
-                        "actived_flg": true,
-                        "deleted_flg": false,
-                        "created_by": 32,
-                        "updated_by": 32,
                         "survey_id": "42ce1633-52a0-4b24-8371-439ff62b30d2",
                         "sub_type": "SURVEY",
                         "created_at": "2020-11-18T04:52:40.049Z",
                         "updated_at": "2020-11-18T04:52:40.049Z",
-                        "Answers": [
+                        "answers": [
                         {
                             "id": 157,
                             "question_id": 42,
                             "text": "",
-                            "is_correct_flg": true,
                             "created_at": "2020-11-18T04:52:40.210Z",
                             "updated_at": "2020-11-18T04:52:40.210Z"
                         },
@@ -71,7 +63,6 @@ module.exports = router;
                             "id": 163,
                             "question_id": 42,
                             "text": null,
-                            "is_correct_flg": true,
                             "created_at": "2020-11-18T08:24:52.517Z",
                             "updated_at": "2020-11-18T08:24:52.517Z"
                         }
@@ -84,20 +75,15 @@ module.exports = router;
                         "category_type": "ANSWER_NOW",
                         "points": 1,
                         "estimate_time": 11,
-                        "actived_flg": true,
-                        "deleted_flg": false,
-                        "created_by": 32,
-                        "updated_by": 96,
                         "survey_id": "42ce1633-52a0-4b24-8371-439ff62b30d2",
                         "sub_type": "SURVEY",
                         "created_at": "2020-11-18T04:52:40.294Z",
                         "updated_at": "2020-11-18T08:45:53.965Z",
-                        "Answers": [
+                        "answers": [
                         {
                             "id": 158,
                             "question_id": 43,
                             "text": "yes ja",
-                            "is_correct_flg": true,
                             "created_at": "2020-11-18T04:52:40.458Z",
                             "updated_at": "2020-11-18T08:45:53.975Z"
                         }

dd334a7ae1f08a7b18bd1d54f757ea246fe87103
Son <sontt@blockchainlabs.asia>
2020-12-21 19:32:50 +0800

    Enable CENNZ CPAY

--- app/feature/get-wallet/get-wallet.controller.js
+++ app/feature/get-wallet/get-wallet.controller.js
@@ -111,8 +111,8 @@ module.exports = {
       let { count: total, rows: wallet_priv_keys } = await WalletPrivateKey.findAndCountAll({ offset: off, limit: lim, where: where, include: include, order: order });
 
       //TODO: hard code for hotfix disable ['CENNZ', 'CPAY'];
-      let ignoreCoin = ['CENNZ', 'CPAY'];
-      wallet_priv_keys = wallet_priv_keys.filter(x => !ignoreCoin.includes(x.currency.symbol));
+      //let ignoreCoin = ['CENNZ', 'CPAY'];
+      //wallet_priv_keys = wallet_priv_keys.filter(x => !ignoreCoin.includes(x.currency.symbol));
       return res.ok({
         items: walletPrivateKeyMapper(wallet_priv_keys),
         offset: off,
